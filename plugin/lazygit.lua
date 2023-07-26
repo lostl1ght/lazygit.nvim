35fd45949c2308d15644c3945014231a13337c83
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2023-07-26 12:52:34 +0300

    refactor: simplify callback

--- plugin/lazygit.lua
+++ plugin/lazygit.lua
@@ -1,10 +1,4 @@
-local function callback(args)
+vim.api.nvim_create_user_command('Lg', function(args)
   local path = args.args ~= '' and args.args or nil
-  if args.bang then
-    require('lazygit').open(path, false)
-  else
-    require('lazygit').open(path)
-  end
-end
-
-vim.api.nvim_create_user_command('Lg', callback, { nargs = '?', desc = 'Lazygit', bang = true })
+  require('lazygit').open(path, not args.bang)
+end, { nargs = '?', desc = 'Lazygit', bang = true })

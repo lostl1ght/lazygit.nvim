53ce58648fe774c5d953ab93f630d0d8ae29590f
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-10 22:27:02 +0300

    feat: add neovim version check

--- lua/lazygit/health.lua
+++ lua/lazygit/health.lua
@@ -2,6 +2,11 @@ local M = {}
 
 M.check = function()
   vim.health.start('lazygit.nvim')
+  if vim.fn.has('nvim-0.10') == 1 then
+    vim.health.ok('Neovim version is >=0.10')
+  else
+    vim.health.error('lazygit.nvim requires neovim >=0.10')
+  end
   if vim.fn.executable('lazygit') == 1 then
     vim.health.ok('Lazygit is installed')
   else

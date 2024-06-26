fa08d46c6c1bf8b629e171e0acead053f61b4259
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-06-26 21:28:14 +0300

    fix: add loading guard to plugin

--- plugin/lazygit.lua
+++ plugin/lazygit.lua
@@ -1,4 +1,10 @@
+if vim.g.loaded_lazygit then
+  return
+end
+
 vim.api.nvim_create_user_command('Lg', function(args)
   local path = args.args ~= '' and args.args or nil
   require('lazygit').open(path, not args.bang)
 end, { nargs = '?', desc = 'Lazygit', bang = true })
+
+vim.g.loaded_lazygit = true

f72b2841761e29fba5ac8ee3a1d2d20a281b0762
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2024-07-04 00:28:08 +0300

    health: style checks

--- lua/lazygit/health.lua
+++ lua/lazygit/health.lua
@@ -3,12 +3,12 @@ local M = {}
 function M.check()
   vim.health.start('lazygit.nvim')
   if vim.fn.has('nvim-0.10.0') == 1 then
-    vim.health.ok('Neovim version is >=0.10.0')
+    local version = vim.version()
+    vim.health.ok(('{neovim} version `%d.%d.%d`'):format(version.major, version.minor, version.patch))
   else
     vim.health.error('lazygit.nvim requires neovim >=0.10.0')
   end
   if vim.fn.executable('lazygit') == 1 then
-    vim.health.ok('Lazygit is installed')
     local function get_version()
       local cmd = { 'lazygit', '-v' }
       if vim.system then
@@ -22,11 +22,11 @@ function M.check()
     local version = { major = tonumber(major), minor = tonumber(minor), patch = tonumber(patch) }
     if version.major > 0 or version.minor >= 38 then
       vim.health.ok(
-        ('Lazygit version is %d.%d.%d'):format(version.major, version.minor, version.patch)
+        ('{lazygit} version `%d.%d.%d`'):format(version.major, version.minor, version.patch)
       )
     else
       vim.health.error(
-        ('lazygit.nvim requires lazygit >=0.38.0 (%d.%d.%d detected)'):format(
+        ('*lazygit.nvim* requires {lazygit} version `0.38.0` (`%d.%d.%d` detected)'):format(
           version.major,
           version.minor,
           version.patch
